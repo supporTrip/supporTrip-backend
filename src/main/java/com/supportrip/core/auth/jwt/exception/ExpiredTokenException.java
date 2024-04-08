7d3445a5e8ba5f4fbbd581b3099040bcf60fcacd
yjlee0235 <yjlee0235@naver.com>
2024-04-08 16:43:47 +0900

    ST-98 Feat: AccessToken, IdToken의 만료 여부 판단 후 payload를 추출하는 기능 추가

--- src/main/java/com/supportrip/core/auth/jwt/exception/ExpiredTokenException.java
+++ src/main/java/com/supportrip/core/auth/jwt/exception/ExpiredTokenException.java
@@ -2,9 +2,10 @@ package com.supportrip.core.auth.jwt.exception;
 
 import com.supportrip.core.error.ErrorInfo;
 import com.supportrip.core.error.exception.BusinessException;
+import org.springframework.boot.logging.LogLevel;
 
 public class ExpiredTokenException extends BusinessException {
     public ExpiredTokenException() {
-        super(ErrorInfo.EXPIRED_TOKEN);
+        super(ErrorInfo.EXPIRED_TOKEN, LogLevel.INFO);
     }
 }
--- src/main/java/com/supportrip/core/auth/jwt/exception/InvalidTokenTypeException.java
+++ src/main/java/com/supportrip/core/auth/jwt/exception/InvalidTokenTypeException.java
@@ -2,9 +2,10 @@ package com.supportrip.core.auth.jwt.exception;
 
 import com.supportrip.core.error.ErrorInfo;
 import com.supportrip.core.error.exception.BusinessException;
+import org.springframework.boot.logging.LogLevel;
 
 public class InvalidTokenTypeException extends BusinessException {
     public InvalidTokenTypeException() {
-        super(ErrorInfo.INVALID_TOKEN_TYPE);
+        super(ErrorInfo.INVALID_TOKEN_TYPE, LogLevel.INFO);
     }
 }
