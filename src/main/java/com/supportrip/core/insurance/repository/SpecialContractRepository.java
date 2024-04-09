4d5ee98a3e90da01020813f9c466597ca7709dfd
zzzdks760 <zzzdks760@naver.com>
2024-04-09 19:37:50 +0900

    Feat: SpecialContractRepository 여행자 보험 아이디로 특약 상위3개 조회 추가

--- src/main/java/com/supportrip/core/insurance/repository/SpecialContractRepository.java
+++ src/main/java/com/supportrip/core/insurance/repository/SpecialContractRepository.java
@@ -1,2 +1,14 @@
-package com.supportrip.core.insurance.repository;public interface SpecialContractRepository {
+package com.supportrip.core.insurance.repository;
+
+import com.supportrip.core.insurance.domain.SpecialContract;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SpecialContractRepository extends JpaRepository<SpecialContract, Long> {
+
+    List<SpecialContract> findByFlightInsuranceId(Long id, Pageable pageable);
 }
