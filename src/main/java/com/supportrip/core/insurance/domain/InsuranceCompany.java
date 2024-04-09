9959e7b66ecdad3069cacea3e8d3d95ee3db7b74
zzzdks760 <zzzdks760@naver.com>
2024-04-09 19:27:12 +0900

    Feat: InsuranceCompany 엔티티 수정

--- src/main/java/com/supportrip/core/insurance/domain/InsuranceCompany.java
+++ src/main/java/com/supportrip/core/insurance/domain/InsuranceCompany.java
@@ -1,7 +1,9 @@
 package com.supportrip.core.insurance.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,11 +13,30 @@ import lombok.NoArgsConstructor;
 public class InsuranceCompany {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "insurance_company_id")
     private Long id;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private String logoImageUrl;
 
+    @NotNull
     private String InsuranceCompanyUrl;
+
+    @Builder
+    private InsuranceCompany(Long id, String name, String logoImageUrl, String insuranceCompanyUrl) {
+        this.id = id;
+        this.name = name;
+        this.logoImageUrl = logoImageUrl;
+        InsuranceCompanyUrl = insuranceCompanyUrl;
+    }
+
+    //==정적 팩토리 메서드==//
+    public static InsuranceCompany from(String name) {
+        return builder()
+                .name(name)
+                .build();
+    }
 }
