a060b4400b92f276f29e90be250d06cd56897e22
zzzdks760 <zzzdks760@naver.com>
2024-04-09 19:27:58 +0900

    Feat: InsuranceSubscription 엔티티 수정

--- src/main/java/com/supportrip/core/insurance/domain/InsuranceSubscription.java
+++ src/main/java/com/supportrip/core/insurance/domain/InsuranceSubscription.java
@@ -1,6 +1,7 @@
 package com.supportrip.core.insurance.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,16 +15,19 @@ public class InsuranceSubscription {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "insurance_subscription_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "travel_insurance_id")
-    private TravelInsurance travelInsurance;
+    @JoinColumn(name = "flight_insurance_id")
+    private FlightInsurance flightInsurance;
 
 //    private Users users;
 
+    @NotNull
     private LocalDateTime coverageStartAt;
 
+    @NotNull
     private LocalDateTime coverageEndAt;
 
     private LocalDateTime createdAt;
