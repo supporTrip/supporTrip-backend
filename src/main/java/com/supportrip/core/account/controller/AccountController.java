988ee341cc11c697e6318e1418cf1ca85dfa4c85
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-04-18 15:15:40 +0900

    ST-51 Feat: 외화계좌 조회 api 개발

--- src/main/java/com/supportrip/core/account/controller/AccountController.java
+++ src/main/java/com/supportrip/core/account/controller/AccountController.java
@@ -1,15 +1,13 @@
 package com.supportrip.core.account.controller;
 
 import com.supportrip.core.account.dto.request.GenerateForeignAccountRequest;
+import com.supportrip.core.account.dto.response.ForeignAccountInfoListResponse;
 import com.supportrip.core.account.dto.response.GenerateForeignAccountResponse;
 import com.supportrip.core.account.service.AccountService;
 import com.supportrip.core.auth.domain.OidcUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +19,10 @@ public class AccountController {
     public GenerateForeignAccountResponse generateForeignAccount(@AuthenticationPrincipal OidcUser oidcUser
             , @RequestBody GenerateForeignAccountRequest generateForeignAccountRequest){
         return accountService.generateForeignAccount(oidcUser.getUserId(), generateForeignAccountRequest);
+    }
+
+    @GetMapping("/details")
+    public ForeignAccountInfoListResponse getForeignAccountInfo(@AuthenticationPrincipal OidcUser oidcUser){
+        return accountService.getForeignAccountInfo(oidcUser.getUserId());
     }
 }
--- src/main/java/com/supportrip/core/account/service/AccountService.java
+++ src/main/java/com/supportrip/core/account/service/AccountService.java
@@ -2,16 +2,29 @@ package com.supportrip.core.account.service;
 
 import com.supportrip.core.account.domain.Bank;
 import com.supportrip.core.account.domain.ForeignAccount;
+import com.supportrip.core.account.domain.ForeignAccountTransaction;
+import com.supportrip.core.account.domain.ForeignCurrencyWallet;
 import com.supportrip.core.account.dto.request.GenerateForeignAccountRequest;
+import com.supportrip.core.account.dto.response.ForeignAccountInfoListResponse;
+import com.supportrip.core.account.dto.response.ForeignAccountInfoResponse;
+import com.supportrip.core.account.dto.response.ForeignAccountTransactionDetailResponse;
 import com.supportrip.core.account.dto.response.GenerateForeignAccountResponse;
 import com.supportrip.core.account.exception.ForeignAccountDuplicateException;
 import com.supportrip.core.account.repository.BankRepository;
 import com.supportrip.core.account.repository.ForeignAccountRepository;
+import com.supportrip.core.account.repository.ForeignCurrencyWalletRepository;
+import com.supportrip.core.exchange.domain.Currency;
+import com.supportrip.core.exchange.repository.CurrencyRepository;
 import com.supportrip.core.user.domain.User;
 import com.supportrip.core.user.exception.UserNotFoundException;
 import com.supportrip.core.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.supportrip.core.account.repository.ForeignAccountTransactionRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +32,9 @@ public class AccountService {
     private final UserRepository userRepository;
     private final BankRepository bankRepository;
     private final ForeignAccountRepository foreignAccountRepository;
+    private final ForeignCurrencyWalletRepository foreignCurrencyWalletRepository;
+    private final ForeignAccountTransactionRepository foreignAccountTransactionRepository;
+    private final CurrencyRepository currencyRepository;
     public GenerateForeignAccountResponse generateForeignAccount(Long userId, GenerateForeignAccountRequest generateForeignAccountRequest) {
 
         User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
@@ -29,6 +45,52 @@ public class AccountService {
         Bank bank = bankRepository.findByName(generateForeignAccountRequest.getBankName());
         ForeignAccount foreignAccount = ForeignAccount.of(user, bank, generateForeignAccountRequest.getAccountNumber());
         foreignAccountRepository.save(foreignAccount);
+
+        List<Currency> currencyList = currencyRepository.findAll();
+
+        for(Currency currency : currencyList){
+            ForeignCurrencyWallet wallet = ForeignCurrencyWallet.of(foreignAccount, currency, 0.0);
+            foreignCurrencyWalletRepository.save(wallet);
+        }
+
         return GenerateForeignAccountResponse.of(foreignAccount.getId());
+    }
+
+    public ForeignAccountInfoListResponse getForeignAccountInfo(Long userId) {
+
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        Optional<ForeignAccount> optionalForeignAccount = foreignAccountRepository.findByUser(user);
+        if (optionalForeignAccount.isEmpty()) {
+            return ForeignAccountInfoListResponse.of(false, null);
+        }
+
+        ForeignAccount foreignAccount = optionalForeignAccount.get();
+
+        List<ForeignCurrencyWallet> foreignCurrencyWallets = foreignCurrencyWalletRepository.findByForeignAccountAndTotalAmountGreaterThan(foreignAccount,0.0);
+
+        if(foreignCurrencyWallets == null)
+            return ForeignAccountInfoListResponse.of(true, null);
+
+        List<ForeignAccountInfoResponse> accountInfoResponses = new ArrayList<>();
+
+        for(ForeignCurrencyWallet wallet : foreignCurrencyWallets){
+            List<ForeignAccountTransactionDetailResponse> details = new ArrayList<>();
+            List<ForeignAccountTransaction> transactions = foreignAccountTransactionRepository.findByForeignCurrencyWalletOrderByCreatedAtDesc(wallet);
+
+            double originTotalAmount = 0.0;
+            double targetTotalAmount = wallet.getTotalAmount();
+
+            for(ForeignAccountTransaction transaction : transactions){
+                details.add(ForeignAccountTransactionDetailResponse.of(transaction));
+                originTotalAmount += transaction.getAmount() * transaction.getTargetExchangeRate();
+            }
+
+            double averageRate = originTotalAmount / targetTotalAmount;
+
+            accountInfoResponses.add(ForeignAccountInfoResponse.of(wallet, averageRate, details));
+        }
+
+        return ForeignAccountInfoListResponse.of(true, accountInfoResponses);
     }
 }
