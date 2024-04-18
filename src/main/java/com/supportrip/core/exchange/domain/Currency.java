f2aca3592d86234ca4548a1d9c6f2fea031372ee
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-04-18 15:14:13 +0900

    ST-51 Feat: currency, country 엔티티, 레포 생성

--- src/main/java/com/supportrip/core/exchange/domain/Currency.java
+++ src/main/java/com/supportrip/core/exchange/domain/Currency.java
@@ -2,6 +2,7 @@ package com.supportrip.core.exchange.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,4 +14,35 @@ public class Currency {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "currency_name")
+    private String name;
+
+    @Column(name = "currency_unit")
+    private String unit;
+
+    @Column(name = "currency_sign")
+    private String sign;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Currency(Long id, Country country, String name, String unit, String sign) {
+        this.id = id;
+        this.country = country;
+        this.name = name;
+        this.unit = unit;
+        this.sign = sign;
+    }
+
+    public static Currency of(Country country, String name, String unit, String sign){
+        return Currency.builder()
+                .country(country)
+                .name(name)
+                .unit(unit)
+                .sign(sign)
+                .build();
+    }
 }
