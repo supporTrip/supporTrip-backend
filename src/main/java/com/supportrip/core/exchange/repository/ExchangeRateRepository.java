6d6f7c12f29a006f0f4e6561070f05b118455386
yjlee0235 <yjlee0235@naver.com>
2024-04-23 15:22:56 +0900

    ST-141 Feat: 특정 통화의 이전 3개월간 평균 환율 조회 기능 추가

--- src/main/java/com/supportrip/core/exchange/repository/ExchangeRateRepository.java
+++ src/main/java/com/supportrip/core/exchange/repository/ExchangeRateRepository.java
@@ -4,8 +4,12 @@ import com.supportrip.core.exchange.domain.Currency;
 import com.supportrip.core.exchange.domain.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
     Optional<ExchangeRate> findLatestExchangeByTargetCurrency(Currency targetCurrency);
+
+    List<ExchangeRate> findByTargetCurrencyAndCreatedAtBetween(Currency targetCurrency, LocalDateTime startedAt, LocalDateTime endedAt);
 }
