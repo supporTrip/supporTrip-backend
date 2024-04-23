241643cf7592cdadf40bcf7dda255868385a8f33
zzzdks760 <zzzdks760@naver.com>
2024-04-23 11:30:51 +0900

    ST-92 Feat: 보험가입이력 필드 추가

--- src/main/java/com/supportrip/core/insurance/domain/InsuranceSubscription.java
+++ src/main/java/com/supportrip/core/insurance/domain/InsuranceSubscription.java
@@ -11,12 +11,15 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "insurance_subscription")
-public class InsuranceSubscription {
+public class InsuranceSubscription extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "total_premium")
+    private int totalPremium;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "flight_insurance_id")
@@ -32,7 +35,11 @@ public class InsuranceSubscription {
     @Column(name = "coverage_end_at", nullable = false)
     private LocalDateTime coverageEndAt;
 
-    private LocalDateTime createdAt;
+    @Column(name = "coverage_details_terms_content", nullable = false)
+    private boolean coverageDetailsTermsContent;
+
+    @Column(name = "consent_personal_info", nullable = false)
+    private boolean consentPersonalInfo;
 
     private LocalDateTime updatedAt;
 }
