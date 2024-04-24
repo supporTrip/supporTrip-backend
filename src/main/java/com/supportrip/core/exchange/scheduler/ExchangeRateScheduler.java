97a1685e884a91b7b3d6bbb0479a01937c38f626
yjlee0235 <yjlee0235@naver.com>
2024-04-24 12:05:31 +0900

    ST-141 Feat: 매일 12/18시에 자동으로 환전 진행하는 기능 추가

--- src/main/java/com/supportrip/core/exchange/scheduler/ExchangeRateScheduler.java
+++ src/main/java/com/supportrip/core/exchange/scheduler/ExchangeRateScheduler.java
@@ -1,8 +1,11 @@
 package com.supportrip.core.exchange.scheduler;
 
 import com.supportrip.core.exchange.domain.ExchangeRate;
+import com.supportrip.core.exchange.domain.ExchangeTrading;
 import com.supportrip.core.exchange.repository.ExchangeRateRepository;
+import com.supportrip.core.exchange.repository.ExchangeTradingRepository;
 import com.supportrip.core.exchange.scheduler.dto.KoreaExImExchangeRateResponse;
+import com.supportrip.core.exchange.service.ExchangeStrategyManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 
+import static com.supportrip.core.exchange.domain.TradingStatus.IN_PROGRESS;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -20,15 +25,30 @@ public class ExchangeRateScheduler {
     private final KoreaExImExchangeRateMapper exchangeRateMapper;
     private final ExchangeRateRepository exchangeRateRepository;
 
-    // 수출입은행 Open API로부터 매일 정오에 1번 환율 정보를 가져와 DB에 저장
+    private final ExchangeStrategyManager exchangeStrategyManager;
+
+    private final ExchangeTradingRepository exchangeTradingRepository;
+
+    // 매일 12/18시 2번 자동 환전을 진행
     @Transactional
-    @Scheduled(cron = "0 0 12 ? * MON-FRI")
-    public void fetchAndStoreExchangeRate() {
-        LocalDate searchDate = LocalDate.now();
+    @Scheduled(cron = "0 0 12/18 ? * MON-FRI")
+    public void dailyExchange() {
+        LocalDate today = LocalDate.now();
+        fetchAndStoreExchangeRate(today);
+
+        List<ExchangeTrading> exchangeTradings = exchangeTradingRepository.findByStatus(IN_PROGRESS);
+
+        log.info("{} ExchangeTradings proceed with currency exchange.", exchangeTradings.size());
+        exchangeTradings.forEach(exchangeTrading ->
+                exchangeStrategyManager.executeExchangeStrategy(exchangeTrading, today)
+        );
+    }
+
+    private void fetchAndStoreExchangeRate(LocalDate searchDate) {
         List<KoreaExImExchangeRateResponse> responses = exchangeRateClient.fetchExchangeRate(searchDate);
 
         if (responses.isEmpty()) {
-            return;
+            throw new IllegalStateException("오늘은 수출입은행으로부터 환율 정보를 가져올 수 없는 일자입니다. 비영업일이거나 영업 시간 이전일 수 있습니다.");
         }
 
         List<ExchangeRate> exchangeRates = responses.stream()
