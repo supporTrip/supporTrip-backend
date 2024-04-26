167b6d873fea63f11108fc2bed177df26d5e3e63
yjlee0235 <yjlee0235@naver.com>
2024-04-26 10:34:04 +0900

    ST-29 Refactor: PhoneVerificationController 코드를 UserController에 병합
    ST-57 Merge: dev merge 충돌 해결

--- src/main/java/com/supportrip/core/user/controller/UserController.java
+++ src/main/java/com/supportrip/core/user/controller/UserController.java
@@ -2,24 +2,30 @@ package com.supportrip.core.user.controller;
 
 import com.supportrip.core.auth.domain.OidcUser;
 import com.supportrip.core.common.SimpleIdResponse;
+import com.supportrip.core.common.SmsService;
 import com.supportrip.core.insurance.dto.UserInfoResponse;
+import com.supportrip.core.user.domain.PhoneVerification;
 import com.supportrip.core.user.domain.User;
-import com.supportrip.core.user.dto.request.SignUpRequest;
+import com.supportrip.core.user.dto.InitiatePhoneVerificationRequest;
+import com.supportrip.core.user.dto.SignUpRequest;
+import com.supportrip.core.user.dto.VerifyPhoneVerificationCodeRequest;
+import com.supportrip.core.user.service.PhoneVerificationService;
 import com.supportrip.core.user.dto.response.MyPageProfileResponse;
 import com.supportrip.core.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final SmsService smsService;
+    private final PhoneVerificationService phoneVerificationService;
 
     @PutMapping("/api/v1/users/signup")
     public SimpleIdResponse signUp(@Valid @RequestBody SignUpRequest request,
@@ -38,5 +44,24 @@ public class UserController {
     public MyPageProfileResponse getUserProfile(@AuthenticationPrincipal OidcUser oidcUser) {
         User user = userService.getUser(oidcUser.getUserId());
         return userService.getUserProfile(user);
+    }
+
+    @PutMapping("/api/v1/users/phone-verification")
+    public void initiatePhoneVerification(@AuthenticationPrincipal OidcUser oidcUser,
+                                          @RequestBody @Valid InitiatePhoneVerificationRequest request) {
+        LocalDateTime now = LocalDateTime.now();
+        PhoneVerification phoneVerification =
+                phoneVerificationService.createOrRenewPhoneVerification(oidcUser.getUserId(), now);
+        smsService.sendOne(makePhoneVerificationMessage(phoneVerification.getCode()), request.getSmsPhoneNumber());
+    }
+
+    @PatchMapping("/api/v1/users/phone-verification")
+    public void verifyPhoneVerificationCode(@AuthenticationPrincipal OidcUser oidcUser,
+                                            @RequestBody @Valid VerifyPhoneVerificationCodeRequest request) {
+        phoneVerificationService.verifyCode(oidcUser.getUserId(), request.getCode());
+    }
+
+    private String makePhoneVerificationMessage(String code) {
+        return String.format("[서포트립] 인증번호는 [%s] 입니다. 본인 확인을 위해 2분 내에 입력해주세요.", code);
     }
 }
