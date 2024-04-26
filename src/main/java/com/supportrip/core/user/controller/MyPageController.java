9c2c7038c5fb51efca88419c8052070e6dff7e9b
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-04-26 15:02:13 +0900

    ST-148 Feat: 마이페이지 포인트내역 조회 기능 개발

--- src/main/java/com/supportrip/core/user/controller/MyPageController.java
+++ src/main/java/com/supportrip/core/user/controller/MyPageController.java
@@ -1,5 +1,6 @@
 package com.supportrip.core.user.controller;
 
+import com.supportrip.core.account.dto.response.PointListResponse;
 import com.supportrip.core.auth.domain.OidcUser;
 import com.supportrip.core.common.SimpleIdResponse;
 import com.supportrip.core.user.domain.User;
@@ -28,5 +29,11 @@ public class MyPageController {
     public SimpleIdResponse modifiyUserProfile(@AuthenticationPrincipal OidcUser oidcUser, @RequestBody UserModifiyRequest request){
         User user = userService.getUser(oidcUser.getUserId());
         return userService.modifiyUserProfile(user, request);
+    }
+
+    @GetMapping("/points")
+    public PointListResponse getPointList(@AuthenticationPrincipal OidcUser oidcUser){
+        User user = userService.getUser(oidcUser.getUserId());
+        return userService.getPointList(user);
     }
 }
