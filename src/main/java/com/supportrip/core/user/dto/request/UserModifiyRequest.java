15998558de9c0e7bcdd6dc5da2cec942c53fe1b9
yjlee0235 <yjlee0235@naver.com>
2024-04-29 16:46:21 +0900

    ST-157 Feat: 유저 정보 수정시 유저 로그 저장

--- src/main/java/com/supportrip/core/user/dto/request/UserModifiyRequest.java
+++ src/main/java/com/supportrip/core/user/dto/request/UserModifiyRequest.java
@@ -2,8 +2,10 @@ package com.supportrip.core.user.dto.request;
 
 import com.supportrip.core.account.dto.request.BankRequest;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class UserModifiyRequest {
     private String phoneNumber;
     private BankRequest bankAccounts;
