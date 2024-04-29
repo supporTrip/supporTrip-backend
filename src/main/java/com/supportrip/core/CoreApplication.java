15b70ffec3355476f0223960b1dceb87900cb152
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-04-29 12:05:56 +0900

    ST-59 Fix: Config로 설정코드 이동

--- src/main/java/com/supportrip/core/CoreApplication.java
+++ src/main/java/com/supportrip/core/CoreApplication.java
@@ -3,9 +3,8 @@ package com.supportrip.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CoreApplication {
 
--- src/main/java/com/supportrip/core/config/FeignClientConfig.java
+++ src/main/java/com/supportrip/core/config/FeignClientConfig.java
@@ -1,9 +1,10 @@
 package com.supportrip.core.config;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
+@EnableFeignClients(basePackages = "com.supportrip.core")
 public class FeignClientConfig {
 
 }
