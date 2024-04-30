cd219381bec60a6e9bfc78524ad794379c81cd2e
yjlee0235 <yjlee0235@naver.com>
2024-04-30 11:43:54 +0900

    Fix: SMS 인증 실제로 진행하도록 수정

--- src/main/java/com/supportrip/core/user/controller/UserController.java
+++ src/main/java/com/supportrip/core/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.supportrip.core.account.domain.PointWallet;
 import com.supportrip.core.account.service.PointWalletService;
 import com.supportrip.core.auth.domain.OidcUser;
 import com.supportrip.core.common.SimpleIdResponse;
+import com.supportrip.core.common.SmsService;
 import com.supportrip.core.insurance.dto.UserInfoResponse;
 import com.supportrip.core.log.domain.UserLog;
 import com.supportrip.core.log.dto.UserLogListResponse;
@@ -13,9 +14,9 @@ import com.supportrip.core.user.domain.User;
 import com.supportrip.core.user.dto.InitiatePhoneVerificationRequest;
 import com.supportrip.core.user.dto.VerifyPhoneVerificationCodeRequest;
 import com.supportrip.core.user.dto.admin.AdminUserDetailResponse;
+import com.supportrip.core.user.dto.admin.AdminUserEnabledUpdateRequest;
 import com.supportrip.core.user.dto.admin.AdminUserEnabledUpdatedResponse;
 import com.supportrip.core.user.dto.admin.AdminUserResponse;
-import com.supportrip.core.user.dto.admin.AdminUserEnabledUpdateRequest;
 import com.supportrip.core.user.dto.request.PinNumberVerificationRequest;
 import com.supportrip.core.user.dto.request.SignUpRequest;
 import com.supportrip.core.user.dto.response.CurrentUserPointResponse;
@@ -35,10 +36,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    //    private final SmsService smsService;
     private final PhoneVerificationService phoneVerificationService;
     private final PointWalletService pointWalletService;
     private final UserLogService userLogService;
+    private final SmsService smsService;
 
     @PutMapping("/api/v1/users/signup")
     public SimpleIdResponse signUp(@Valid @RequestBody SignUpRequest request,
@@ -61,8 +62,7 @@ public class UserController {
         LocalDateTime now = LocalDateTime.now();
         PhoneVerification phoneVerification =
                 phoneVerificationService.createOrRenewPhoneVerification(oidcUser.getUserId(), now);
-        // TODO: 배포를 위한 주석으로 프로젝트 완료시 주석 해제 필요
-//        smsService.sendOne(makePhoneVerificationMessage(phoneVerification.getCode()), request.getSmsPhoneNumber());
+        smsService.sendOne(makePhoneVerificationMessage(phoneVerification.getCode()), request.getSmsPhoneNumber());
     }
 
     @PatchMapping("/api/v1/users/phone-verification")
