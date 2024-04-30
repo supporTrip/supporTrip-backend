dd7f1620d9858fd2b7c7e07873b52603ab3a8abe
yjlee0235 <yjlee0235@naver.com>
2024-04-30 17:38:21 +0900

    Feat: PinNumber 단방향 암호화 적용

--- src/main/java/com/supportrip/core/config/SecurityConfig.java
+++ src/main/java/com/supportrip/core/config/SecurityConfig.java
@@ -13,7 +13,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 
@@ -56,5 +58,10 @@ public class SecurityConfig {
     @Bean
     public AesBytesEncryptor aesBytesEncryptor() {
         return new AesBytesEncryptor("${jwt.secret}", "70726574657374");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
